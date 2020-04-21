5e90dcbf2f77085675fa676a41dd2d81a5ed4b14
Divya Elangovan <34182846+Divya2107@users.noreply.github.com>
2020-04-21 15:05:20 +0530

    Add files via upload
    W2D2

--- Createlead.java
+++ Createlead.java
@@ -1,10 +1,7 @@
-package week2.day3;
-
-import java.util.List;
+package week1.day2;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
 
 public class Createlead
 {
@@ -28,26 +25,12 @@ public class Createlead
 		driver.findElementById("createLeadForm_companyName").sendKeys("Accenture");
 		driver.findElementById("createLeadForm_firstName").sendKeys("Div");
 		driver.findElementById("createLeadForm_lastName").sendKeys("Elan");
-		//driver.findElementByClassName("smallSubmit").click();
-		//String title = driver.getTitle();
-		//System.out.println(title);
-		//driver.close();
+		driver.findElementByClassName("smallSubmit").click();
+		String title = driver.getTitle();
+		System.out.println(title);
+		driver.close();
 		
 		
-		WebElement dropdown1= driver.findElementById("createLeadForm_dataSourceId");
-		Select dd1 =  new Select(dropdown1);
-		
-		dd1.selectByVisibleText("Cold Call");
-		
-		WebElement dropdown2= driver.findElementById("createLeadForm_industryEnumId");
-		Select dd2 = new Select(dropdown2);
-		
-		 List<WebElement> options= dd2.getOptions();
-		int count = options.size();
-		dd2.selectByIndex(count-1);
-		//String dropdown= dropdown2.getText();
-		//System.out.println(dropdown);
-				
 		
 	}
 }
