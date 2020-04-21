d37f6a21bd0753b1245d978732d0c25b5038d3e3
Divya Elangovan <34182846+Divya2107@users.noreply.github.com>
2020-04-21 15:53:50 +0530

    Add files via upload

---
+++ DealingWithText.java
@@ -0,0 +1,39 @@
+package week1.day3;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DealingWithText {
+	
+	public static void main(String[] args)
+	
+	{
+		// add system property -> webdriver.chrome.driver
+				System.setProperty("webdriver.chrome.driver", "./drivers/Chromedriver/chromedriver.exe");
+
+				// Open the chrome browser
+				// ChromeDriver (C) -> Written by Selenium Team
+				ChromeDriver driver = new ChromeDriver();
+
+				// Wait (for the element to appear for the max time: 30)
+				driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+				
+				// Load the URL :: get ->
+				driver.get("http://www.leafground.com/pages/Edit.html");
+				
+				driver.findElementById("email").sendKeys("babu@testleaf.com");
+				
+				driver.findElementByXPath("//input[@value='Append ']").sendKeys("babu@testleaf.com",Keys.TAB);
+				
+				String attribute = driver.findElementByName("username").getAttribute("value");
+				System.out.println(attribute);
+				
+				driver.findElementByXPath("(//input[@name='username'])[2]").clear();
+				
+				boolean enabled = driver.findElementByXPath("//input[@disabled='true']").isEnabled();
+				System.out.println(enabled);
+
+}
+}
